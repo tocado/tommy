90b2192bbdabccfcc2ae67b6a1027e8bfdb3c669
GersonTommas <gersontommas@gmail.com>
2016-10-12 21:19:30 -0300

    Toastr...
    Al crear una cuenta, se te dara la bienvenida utilizando AngularJS-Toastr.

--- MyScripts.js
+++ MyScripts.js
@@ -1,5 +1,41 @@
 (function () {
-    var app = angular.module('MyNewModule', []);
+    angular.module('MyNewModule', ['ngAnimate', 'toaster']);
+    var app = angular.module('MyNewModule');
+    /*var app = angular.module('MyNewModule', []);
+    var app2 = angular.module('MyNewModule2', ['ngAnimate', 'toaster']);
+    
+    app2.controller('myController', function ($scope, toaster, $window) {
+       
+        $scope.bar = 'Hi';
+
+        $scope.pop = function () {
+            toaster.success({ title: "title", body: "text1" });
+            toaster.error("title", "text2");
+            toaster.pop({ type: 'wait', title: "title", body: "text" });
+            toaster.pop('success', "title", '<ul><li>Render html</li></ul>', 5000, 'trustedHtml');
+            toaster.pop('error', "title", '<ul><li>Render html</li></ul>', null, 'trustedHtml');
+            toaster.pop('wait', "title", null, null, 'template');
+            toaster.pop('warning', "title", "myTemplate.html", null, 'template');
+            toaster.pop('note', "title", "text");
+            toaster.pop('success', "title", 'Its address is https://google.com.', 5000, 'trustedHtml', function (toaster) {
+                var match = toaster.body.match(/http[s]?:\/\/[^\s]+/);
+                if (match) $window.open(match[0]);
+                return true;
+            });
+            toaster.pop('warning', "Hi ", "{template: 'myTemplateWithData.html', data: 'MyData'}", 15000, 'templateWithData');
+        };
+
+        $scope.goToLink = function (toaster) {
+            var match = toaster.body.match(/http[s]?:\/\/[^\s]+/);
+            if (match) $window.open(match[0]);
+            return true;
+        };
+
+        $scope.clear = function () {
+            toaster.clear();
+        };
+    });
+
     /*
     app.controller('CtrlTrans', ['$translate', '$scope', function($translate, $scope) {
         $scope.changeLanguage = function(langKey) {
@@ -85,11 +121,12 @@
     var mails = [{
 
     }];
-
-    app.controller('NewAccount', function ($scope) {
+    
+    app.controller('NewAccount', function ($scope, toaster) {
         this.People = {};
 
         this.addAccount = function () {
+            toaster.success("Welcome!!! " + this.People.First, "Please, Sing In");
             mails.push(this.People);
             this.People = {};
             $scope.SingUpForm.$setPristine();
